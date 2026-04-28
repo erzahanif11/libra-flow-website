79b9c55d39305d140e2f1de63c4b271f02c46983
erzahanif11 <erzahanif.dev@gmail.com>
2026-04-28 15:54:12 +0700

    Update web.php
    Updating the guest registration route to define the missing named route register.

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin']);
     Route::post('/login', [AuthController::class, 'login']);
 
-    Route::get('/register', [AuthController::class, 'showRegister']);
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 });
 
